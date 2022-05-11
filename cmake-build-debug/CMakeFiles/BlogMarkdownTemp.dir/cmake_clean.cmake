97db875c3a21a188558a8b58e2f1a7643354e5da
RoseAlice2018 <1505243015@qq.com>
2022-05-11 17:57:28 +0800

    first commit

---
+++ cmake-build-debug/CMakeFiles/BlogMarkdownTemp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "BlogMarkdownTemp"
+  "BlogMarkdownTemp.pdb"
+  "CMakeFiles/BlogMarkdownTemp.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BlogMarkdownTemp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
